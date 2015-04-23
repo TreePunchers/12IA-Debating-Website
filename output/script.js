7bda838841ef0f5a69ac75dedc76891915d5ff00
Jacob Cheatley <jacobcheatley@gmail.com>
2015-04-23 20:41:14 +1200

    First commit to GitHub
    Content of the site as my first commit. Lacking content.

---
+++ output/script.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Jacob on 7/04/2015.
+ */
+
+$(document).ready(function() {
+
+    $('.tab-panels .panel-buttons li').on('click', function() {
+        var $panel = $(this).closest('.tab-panels');
+
+        $panel.find('.panel-buttons li.active').removeClass('active');
+        $(this).addClass('active');
+        var panelToShow = $(this).attr('data-panel');
+
+        //Hide current panel
+        $panel.find('.panel.active').fadeIn(300, showNextPanel);
+
+        //Show the next panel
+        function showNextPanel() {
+            $(this).removeClass('active');
+            $('#' + panelToShow).fadeOut(200, function () {
+                $(this).addClass('active');
+            });
+        }
+    });
+
+    $('.tool-tip').tooltip({
+        track: true
+    });
+
+});
